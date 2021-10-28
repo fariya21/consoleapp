9f246cf25369f141fe564ec695ccc81ff8993948
Fariya Wani <Fariya.Wani@sp.com>
2021-10-28 10:25:02 +0530

    k

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,12 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System;
+using System.Text;
 
 namespace HelloWorldTests
 {
     [TestClass]
     public class UnitTest1
     {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
         private const string Expected = "Tests executed!fariya";
         [TestMethod]
         public void TestMethod1()
@@ -15,10 +22,25 @@ namespace HelloWorldTests
             {
                 Console.SetOut(sw);
                 ConsoleApp1.Program.Main();
-                Console.WriteLine("Test...");
                 var result = sw.ToString().Trim();
                 Assert.AreEqual(Expected, result);
+                testContextInstance.WriteLine("test1 passed");
             }
+        }
+
+        [TestCleanup]
+        public void WriteToLogFile()
+        {
+            var testOutcome = TestContext.CurrentTestOutcome;
+            string testName = TestContext.TestName;
+
+            StringBuilder sb = new StringBuilder();
+            string str = testName + ": " + testOutcome.ToString() + "\n";
+            sb.Append(str);
+          
+            File.AppendAllText("C:\\Results\\" + "log.txt", sb.ToString());
+            sb.Clear();
         }
     }
 }
+
